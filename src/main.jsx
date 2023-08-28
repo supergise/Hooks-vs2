e19d5730c9442ad644787113ed376585b71c0cf3
gise <gisellecaballascano@gmail.com>
2023-08-28 10:26:12 -0300

    useContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
@@ -14,14 +15,15 @@ import ReactDOM from "react-dom/client";
 // import { MemoHook } from "./06-memos/MemoHook";
 // import './08-useReducer/intro-reducer';
 // import { Padre } from "./07-tarea-memo/Padre";
-import { TodoApp } from "./08-useReducer/TodoApp";
+// import { TodoApp } from "./08-useReducer/TodoApp";
+import { MainApp } from "./09-useContext/MainApp";
 
 
 import "./index.css";
-
- ReactDOM.createRoot(document.getElementById("root")).render(
-//     // <React.StrictMode>
-    <TodoApp />
-//     // </React.StrictMode>
+        ReactDOM.createRoot(document.getElementById("root")).render(
+            <BrowserRouter>
+                {/* <React.StrictMode> */}
+                    <MainApp />
+                {/* </React.StrictMode> */}
+            </BrowserRouter>    
 );
-
